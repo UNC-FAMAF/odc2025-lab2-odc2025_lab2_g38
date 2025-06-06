584682fc7ee2002791a32f7b88baccc6d1db926c
camila_reyna <145247931+garizo1@users.noreply.github.com>
2025-06-06 17:45:17 -0300

    Update draw_shapes.s

--- ejercicio2/draw_shapes.s
+++ ejercicio2/draw_shapes.s
@@ -73,16 +73,4 @@ y_loop_rect_end:
     ldp x19, x20, [sp], #16
     ret // Retorna de la función.
 
-    .globl draw_pixel // Declara draw_pixel como global para ser llamada desde otros archivos.
 
-// draw_pixel(framebuffer_base (x0), x (x1), y (x2), color (w3))
-// Dibuja un píxel individual en la pantalla.
-draw_pixel:
-    mov x5, #SCREEN_WIDTH
-    mul x4, x2, x5         // x4 = y * SCREEN_WIDTH
-    add x4, x4, x1         // x4 = (y * SCREEN_WIDTH) + x
-    mov x5, #PIXEL_SIZE
-    mul x4, x4, x5         // x4 = (y * SCREEN_WIDTH + x) * 4
-    add x0, x0, x4
-    str w3, [x0]
-    ret
